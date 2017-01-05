8a28d2e3b43876b7c2132d918a993a34786977b1
Jedrzej Siemienski <jedrzej.siemienski@gmail.com>
2017-01-06 00:58:08 +0100

    added returning json as response in controller + dummy POJO for test

--- src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
+++ src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
@@ -3,16 +3,23 @@ package com.jenczuen.restDemo.controllers;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+//import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.jenczuen.restDemo.dtos.TestDTO;
 
 @Path("/employee")
 public class EmployeeController {
 
 	@GET
 	@Path("/{param}")
-	public Response printMessage(@PathParam("param") String msg) {
-		String result = "Restful example : " + msg;
-		return Response.status(200).entity(result).build();
+	@Produces(MediaType.APPLICATION_JSON)
+	public TestDTO printMessage(@PathParam("param") String msg) {
+		TestDTO result = new TestDTO();
+		result.setText("Restful example : " + msg + " " + MediaType.APPLICATION_JSON);
+		result.setNumber(11);
+		return result;
 	}
 	
 }
---
+++ src/main/java/com/jenczuen/restDemo/dtos/TestDTO.java
@@ -0,0 +1,19 @@
+package com.jenczuen.restDemo.dtos;
+
+public class TestDTO {
+	private String text;
+	private int number;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
