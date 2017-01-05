92d6ff8cf18aa81a70632244766cca91333af214
Jedrzej Siemienski <jedrzej.siemienski@gmail.com>
2017-01-05 20:15:54 +0100

    configures jersey (app is going to be run on glassfish server for now), added basic dummy controller

---
+++ src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.jenczuen.restDemo.controllers;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/employee")
+public class EmployeeController {
+
+	@GET
+	@Path("/{param}")
+	public Response printMessage(@PathParam("param") String msg) {
+		String result = "Restful example : " + msg;
+		return Response.status(200).entity(result).build();
+	}
+	
+}
