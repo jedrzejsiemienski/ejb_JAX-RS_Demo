ddd6f0e8c4fce138a6d25cbd193cf96fa9cbaf33
Jedrzej Siemienski <jedrzej.siemienski@gmail.com>
2017-01-06 02:50:53 +0100

    added CORSFilter to avoid cross domain error, added simple index.html, that can display and add employees using jquery

---
+++ main.js
@@ -0,0 +1,54 @@
+$(document).ready(function()
+{
+	updateEmployees();
+	
+	$("#post-employee").click(function()
+	{
+		sendFormData();
+	});
+	
+});
+
+var API_URL = "http://localhost:8080/ejb_JAX-RS_Demo/rest/employee";
+
+function updateEmployees() {
+	$('#employees-list').empty();
+	$.get( API_URL + "/getAll", function( data ) {
+		var list = $('<ul />');
+		jQuery.each(data, function(index, employee) {
+			console.log(index);
+			console.log(employee.name);
+	        $('<li />', {text: index + ' - ' + employee.name}).appendTo(list);
+	    });
+		$('#employees-list').append(list);
+	});
+    
+}
+
+function sendFormData() {
+	var postData = $("#create-employee-form").serializeArray();
+	postData = JSON.stringify(postData);
+	postData = JSON.stringify({"name":"imie"});
+	
+	$.ajax(
+	{
+		url : API_URL + "/save",
+		type: "POST",
+		data : postData,
+	    headers: { 
+	        'Accept': 'application/json',
+	        'Content-Type': 'application/json' 
+	    },
+		success:function(data, textStatus, jqXHR) 
+		{
+			updateEmployees();
+		},
+		error: function(jqXHR, textStatus, errorThrown) 
+		{
+			$("#simple-msg").html(
+				'<pre><code class="prettyprint">AJAX Request Failed<br/> textStatus='
+				+ textStatus + ', errorThrown='+errorThrown+'</code></pre>'
+			);
+		}
+	});	
+}
--- src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
+++ src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.jenczuen.restDemo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -17,18 +20,27 @@ public class EmployeeController {
 	@GET
 	@Path("/getAll")
 	@Produces(MediaType.APPLICATION_JSON)
-	public EmployeeDTO getAll() {
-		EmployeeDTO e = new EmployeeDTO();
-		e.setName("nazwa");
-		return e;
+	public List<EmployeeDTO> getAll() {
+		List<EmployeeDTO> result = new ArrayList<EmployeeDTO>();
+		
+		EmployeeDTO e1 = new EmployeeDTO();
+		e1.setName("nazwa");
+		result.add(e1);
+		
+		EmployeeDTO e2 = new EmployeeDTO();
+		e2.setName("nazwa2");
+		result.add(e2);
+		
+		return result;
 	}
-	
+
 	@POST
 	@Path("/save")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response save(EmployeeDTO e) {
 		String result = "Got employee with name : " + e.getName();
-		return Response.status(201).entity(result).build();
+		//return Response.status(201).entity(result).build();
+		return Response.status(201).build();
 	}
-	
+
 }
