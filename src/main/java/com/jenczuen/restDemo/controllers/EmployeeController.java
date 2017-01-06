904802d23acce362227601507f532ba1ea0a3e12
Jedrzej Siemienski <jedrzej.siemienski@gmail.com>
2017-01-06 01:11:26 +0100

    removed TestDTO, created basic EmployeeDTO, added (dummy) api for adding an employee and fetching all of them

--- src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
+++ src/main/java/com/jenczuen/restDemo/controllers/EmployeeController.java
@@ -1,25 +1,34 @@
 package com.jenczuen.restDemo.controllers;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-//import javax.ws.rs.core.Response;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import com.jenczuen.restDemo.dtos.TestDTO;
+import com.jenczuen.restDemo.dtos.EmployeeDTO;
 
 @Path("/employee")
 public class EmployeeController {
 
 	@GET
-	@Path("/{param}")
+	@Path("/getAll")
 	@Produces(MediaType.APPLICATION_JSON)
-	public TestDTO printMessage(@PathParam("param") String msg) {
-		TestDTO result = new TestDTO();
-		result.setText("Restful example : " + msg + " " + MediaType.APPLICATION_JSON);
-		result.setNumber(11);
-		return result;
+	public EmployeeDTO getAll() {
+		EmployeeDTO e = new EmployeeDTO();
+		e.setName("nazwa");
+		return e;
+	}
+	
+	@POST
+	@Path("/save")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response save(EmployeeDTO e) {
+		String result = "Got employee with name : " + e.getName();
+		return Response.status(201).entity(result).build();
 	}
 	
 }
---
+++ src/main/java/com/jenczuen/restDemo/dtos/EmployeeDTO.java
@@ -0,0 +1,13 @@
+package com.jenczuen.restDemo.dtos;
+
+public class EmployeeDTO {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
